21be23fddeff6e067a61a9dee5d96bc08ae008eb
Haf <haf@protonmail.ch>
2023-08-06 11:49:30 +0200

    flake with devshell

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Lua library for incrementing an arbitrary precision counter";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+  {
+
+    devshell.${system} = pkgs.mkShell {
+      buildInputs = with pkgs; [
+        lua
+      ];
+    };
+  };
+}
